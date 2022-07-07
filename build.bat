635951044f8fb483bedbf3ee7d0facb796127111
Roger Barreto <roger@rkti.com>
2022-07-07 23:12:11 +0100

    Initial changes for Core

--- build.bat
+++ build.bat
@@ -7,21 +7,13 @@ for /D %%p in ("output\*") do rmdir "%%p" /s /q
 
 
 rem build 3D library
-msbuild source\Jitter.sln /p:Configuration=Release /t:Rebuild
+msbuild source\Jitter.Core.sln /p:Configuration=Release /t:Rebuild
 
 rem ready for packaging
-if not exist output\net4 mkdir output\net4
-if not exist output\Portable mkdir output\Portable
-copy source\Jitter\bin\net4\Release\Jitter.dll output\net4\
-copy source\Jitter\bin\net4\Release\Jitter.pdb output\net4\
-copy source\Jitter\bin\net4\Release\Jitter.xml output\net4\
-copy source\Jitter\bin\Portable\Release\Jitter.dll output\Portable\
-copy source\Jitter\bin\Portable\Release\Jitter.pdb output\Portable\
-copy source\Jitter\bin\Portable\Release\Jitter.xml output\Portable\
+if not exist output\netstandard2.0\ mkdir output\netstandard2.0\
 
+copy source\Jitter.Core\bin\Release\netstandard2.0\Jitter.Core.dll output\netstandard2.0\
+copy source\Jitter.Core\bin\Release\netstandard2.0\Jitter.Core.pdb output\netstandard2.0\
+copy source\Jitter.Core\bin\Release\netstandard2.0\Jitter.Core.xml output\netstandard2.0\
 rem package
-nuget pack nuget\Jitter.nuspec -OutputDirectory output
-
-rem package
-xamarin-component package ./component/
-mv component\*.xam output
+nuget pack nuget\Jitter.Core.nuspec -OutputDirectory output
